3d574da543cbc03c817770d77164bda02acc4524
Denis Labaye <denis.labaye@gmail.com>
2012-01-20 19:08:54 +0100

    init

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject denlab-4clojure-exercices "1.0.0-SNAPSHOT"
+  :description "https://github.com/denlab exercices of http://www.4clojure.com"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-stacktrace      "0.2.4"]]
+  :dev-dependencies [[midje "1.3.1"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
