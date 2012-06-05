26702cee3f30cb9aeda5686984234e1e4c75112d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-05 20:21:50 +0200

    Migrate to clojure and midje 1.4.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject denlab-4clojure-exercices/denlab-4clojure-exercices "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-stacktrace "0.2.4"]
                  [org.clojure/tools.cli "0.2.1"]]
   :profiles {:dev
              {:dependencies
-              [[midje "1.3.1"]
+              [[midje "1.4.0"]
                [com.intelie/lazytest
                 "1.0.0-SNAPSHOT"
                 :exclusions
